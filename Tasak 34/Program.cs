6d2294353e8a36fbdadc772b2f4613e50faeb5e9
Ilya <fominiv10@gmail.com>
2022-09-17 17:13:49 +0300

    solved the thirty fourth task

---
+++ Tasak 34/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GetArray (int size)
+{
+
+    var array = new int [size];
+    for (int i = 0; i <array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int AmountEvenNumber (int[] array)
+{
+        int result = 0;
+        for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            result = result +1;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] arr = GetArray(size);
+int sum = AmountEvenNumber (arr);
+Console.WriteLine($"Количество чётных чисел в массиве {sum}");
